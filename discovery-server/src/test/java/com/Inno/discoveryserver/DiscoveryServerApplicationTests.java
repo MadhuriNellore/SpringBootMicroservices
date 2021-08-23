ba5fc37456e9a4b98e62a2e42542063bf30ea34c
Madhuri Nellore <83761621+MadhuriNellore@users.noreply.github.com>
2021-08-23 15:32:32 +0530

    Add files via upload

---
+++ discovery-server/src/test/java/com/Inno/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.Inno.discoveryserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+
+public class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
